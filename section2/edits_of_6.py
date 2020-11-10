2b2f96d76ff899f8921508da44a29d3080e8a958
aleno4ka1998 <alenasnisarenko@gmail.com>
2020-11-10 21:00:22 +0300

    Правка 6 задачи по серёжиным замечаниям
    Правка 6 задачи по серёжиным замечаниям
    
    Требуется определить, бьет ли слон, стоящий на клетке с указанными координатами (номер строки и номер столбца), фигуру, стоящую на другой указанной клетке.
    
    Входные данные
    Вводятся четыре числа: координаты слона и координаты другой фигуры. Координаты - целые числа в интервале от 1 до 8.
    
    Выходные данные
    Требуется вывести слово YES, если слон способен побить фигуру за 1 ход, в противном случае вывести слово NO

---
+++ section2/edits_of_6.py
@@ -0,0 +1,11 @@
+# какое из трёх чисел больше?
+a = int(input())
+b = int(input())
+c = int(input())
+if a >= b and a >= c:
+    print(a)
+else:
+    if b >= a and b >= c:
+        print(b)
+    else:
+        print(c)
