880be750b81c77a5047e64f9a90a97eab130fb71
aleno4ka1998 <alenasnisarenko@gmail.com>
2020-11-23 00:36:12 +0300

    Количество элементов, равных максимуму
    Последовательность состоит из натуральных чисел и завершается числом 0. Всего вводится не более 10000 чисел (не считая завершающего числа 0). Определите, сколько элементов этой последовательности равны ее наибольшему элементу.
    
    Числа, следующие за числом 0, считывать не нужно.

---
+++ section3_1/quantity of maximum numbers.py
@@ -0,0 +1,10 @@
+a = int(input())
+maximum = 0
+quantity_of_maximum = 1
+while a != 0:
+    if a > maximum:
+        maximum = a
+    elif a == maximum:
+        quantity_of_maximum += 1
+    a = int(input())
+print(quantity_of_maximum)
