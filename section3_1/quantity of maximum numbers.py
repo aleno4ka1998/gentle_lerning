7ba65b6c069d7ffe9173cec726eef1d001070aa3
aleno4ka1998 <alenasnisarenko@gmail.com>
2020-11-27 00:50:56 +0300

    Исправленная "Количество элементов, равных максимуму"
    Добавлено обнуление счётчика при обнаружении нового максимума

--- section3_1/quantity of maximum numbers.py
+++ section3_1/quantity of maximum numbers.py
@@ -4,6 +4,7 @@ quantity_of_maximum = 1
 while a != 0:
     if a > maximum:
         maximum = a
+        quantity_of_maximum = 1
     elif a == maximum:
         quantity_of_maximum += 1
     a = int(input())
