42bf634ab48dfca1704d612c969be1c5eb80456e
aleno4ka1998 <alenasnisarenko@gmail.com>
2020-12-03 00:24:59 +0300

    (Исправ) Первое и последнее вхождение
    Вроде полегче код стал

--- section4.2/first and last occurrence.py
+++ section4.2/first and last occurrence.py
@@ -1,8 +1,5 @@
 s = str(input())
-if s.find('f') == s.rfind('f'):
-    if s.find('f') == -1:
-        print('')
-    else:
-        print(s.find('f'))
-else:
+if s.find('f') != s.rfind('f'):
     print(s.find('f'), s.rfind('f'))
+elif s.find('f') != -1:
+    print(s.find('f'))
