ff12c93b3ba98674b36e1c735e02063fb5b070d8
aleno4ka1998 <alenasnisarenko@gmail.com>
2020-12-08 21:45:00 +0300

    upd Первое и последнее вхождение
    Если найти индексы f один раз и работать дальше только с ними, то скорость обработки повысится

--- section4.2/first and last occurrence.py
+++ section4.2/first and last occurrence.py
@@ -1,5 +1,7 @@
 s = str(input())
-if s.find('f') != s.rfind('f'):
-    print(s.find('f'), s.rfind('f'))
-elif s.find('f') != -1:
-    print(s.find('f'))
+f = s.find('f')
+l = s.rfind('f')
+if f != l:
+    print(f, l)
+elif f != -1:
+    print(f)
