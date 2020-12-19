b24fe6cf6c91bced61e4092f2eb1240df2a7fd8a
aleno4ka1998 <alenasnisarenko@gmail.com>
2020-12-20 02:00:11 +0300

    Create most distant point.py

---
+++ section4.3/most distant point.py
@@ -0,0 +1,20 @@
+class Coordinate:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def absolute(self):
+        return(abs(self.x + self.y))
+
+
+n = int(input())
+maximum = 0
+for _ in range(n):
+    x, y = map(int, input().split())
+    coord = Coordinate(x, y)
+    abs_coord = coord.absolute()
+    if abs_coord > maximum:
+        maximum = abs_coord
+        max_x = x
+        max_y = y
+print(max_x, max_y)
