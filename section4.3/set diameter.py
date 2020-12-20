cb905fad59b107fb3d7b26eecdb333f662f5949b
aleno4ka1998 <alenasnisarenko@gmail.com>
2020-12-20 05:45:01 +0300

    Диаметр множества
    Выведите диаметр данного множества – максимальное расстояние между любыми двумя точками.
    
    Создайте структуру Point и сохраните исходные данные в массиве структур Point.
    
    Входные данные
    Программа получает на вход набор точек на плоскости. Сначала задано количество точек n, затем идет последовательность из n строк, каждая из которых содержит два числа: координаты точки. Величина n не превосходит 100000, все исходные координаты – целые числа, не превосходящие 103.
    
    Выходные данные
    Необходимо вывести  диаметр данного множества с точностью в 15 значащих цифр.

---
+++ section4.3/set diameter.py
@@ -0,0 +1,25 @@
+import math
+
+class SetOfCoordinates:
+    X = []
+    Y = []
+    def __init__(self, x, y):
+        self.X.append(x)
+        self.Y.append(y)
+
+    def distance(self, i, j):
+        return math.sqrt((self.X[j] - self.X[i]) ** 2 + (self.Y[j] - self.Y[i]) ** 2)
+
+
+
+n = int(input())
+maximum = 0
+for _ in range(n):
+    x, y = map(int, input().split())
+    mass_coord = SetOfCoordinates(x, y)
+for i in range(0, n - 1):
+    for j in range(i + 1, n):
+        dis = mass_coord.distance(i, j)
+        if dis > maximum:
+            maximum = dis
+print(dis)
